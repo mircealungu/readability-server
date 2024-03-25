447fbed61e6c45b9486554fb1821c6a1e52ce311
Mircea Filip Lungu <mircea.lungu@gmail.com>
2024-03-25 16:39:23 +0200

    added logging to sentry

--- server.js
+++ server.js
@@ -1,81 +1,65 @@
 import express from "express";
-import {advanced_readability_cleanup, basic_readability_cleanup} from "./cleanup.js";
-import {HTML2Text} from "./convert_htm_to_plaintext.js";
+import { advanced_readability_cleanup } from "./cleanup.js";
+import { HTML2Text } from "./convert_htm_to_plaintext.js";
+import * as packageJson from "./package.json" assert { type: "json" };
+import * as Sentry from "@sentry/node";
 
 const app = express();
 
-
-app.get("/ping", async(req, res)=> {
-  res.send("pong");
+// Sentry prefix: START
+Sentry.init({
+  dsn: "https://754bd6621282bfc97d64d3dc12fba591@o176103.ingest.us.sentry.io/4506971631517696",
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Sentry.Integrations.Express({ app }),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 0.1, //  Capture 10% of the transactions
 });
 
-// Define a single endpoint
-app.get("/plain_text", async (req, res) => {
-  const { url } = req.query;
-  console.log(req.query);
+// The request handler must be the first middleware on the app
+app.use(Sentry.Handlers.requestHandler());
 
-  const start = process.hrtime();
-
-  const content = HTML2Text(await advanced_readability_cleanup(url));
-
-  // Send the cleaned up content as the response
-  res.send(content);
-
-  // Calculate the execution time
-  const end = process.hrtime(start);
-  const executionTime = `${end[0]}s ${end[1] / 1000000}ms`;
-
-  console.log(`Request for ${url} took ${executionTime}`);
-});
-
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
+// Sentry prefix: END
 
 app.get("/cleanup", async (req, res) => {
   const { url } = req.query;
   console.log(req.query);
 
-  const start = process.hrtime();
-
   try {
     const html = await advanced_readability_cleanup(url);
     // Send the cleaned up content as the response
-    res.send({html: html, text: HTML2Text(html)});
+    res.send({ html: html, text: HTML2Text(html) });
   } catch (e) {
     res.status(500).send(e.message);
   }
-
-
-
-  // Calculate the execution time
-  const end = process.hrtime(start);
-  const executionTime = `${end[0]}s ${end[1] / 1000000}ms`;
-
-  console.log(`Request for ${url} took ${executionTime}`);
 });
 
-
-
-
-app.get("/old_cleanup", async (req, res) => {
-  const { url } = req.query;
-
-  const start = process.hrtime();
-
-  const content = basic_readability_cleanup(url);
-
-  // Send the cleaned up content as the response
-  res.send(content);
-
-  // Calculate the execution time
-  const end = process.hrtime(start);
-  const executionTime = `${end[0]}s ${end[1] / 1000000}ms`;
-
-  console.log(`Request for ${url} took ${executionTime}`);
+app.get("/ping", async (req, res) => {
+  res.send("pong");
 });
 
+// ----> Sentry Appendix: START
+app.use(Sentry.Handlers.errorHandler());
 
+// Optional fallthrough error handler
+app.use(function onError(err, req, res, next) {
+  // The error id is attached to `res.sentry` to be returned
+  // and optionally displayed to the user for support.
+  res.statusCode = 500;
+  res.end(res.sentry + "\n");
+});
+
+// ----> Sentry Appendix: END
 
 // Start the server
 const port = 3456;
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(
+    `Server (${packageJson.default.version}) is running on http://localhost:${port}`,
+  );
 });
